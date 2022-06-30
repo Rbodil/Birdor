7e24ef9a84ed3cf48ba45fc181efb6fa7bb5ad22
nancywa1 <nancy.wang0717@gmail.com>
2022-06-30 11:38:49 -0600

    fixed typo

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -59,13 +59,12 @@ const resolvers = {
       if (!correctPw) {
         throw new AuthenticationError("Incorrect credentials");
       }
-      console.log(user);
+
       const token = signToken(user);
-      console.log(token)
+
       return { token, user };
     },
     addPost: async (parent, args, context) => {
-      console.log(context.user);
       if (context.user) {
         const post = await Post.create({
           ...args,
